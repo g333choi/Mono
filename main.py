fd869cd23bcf95defd89485b371672047982867e
g333choi <139156847+g333choi@users.noreply.github.com>
2023-10-24 18:55:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from sys import *
 from Interpreter import *
 
 if __name__ == '__main__':
-    parse(argv[1])
+    if argv[1][-5:] == ".mono":
+        parse(argv[1])
+    else:
+        print("ERROR: Wrong file extension.")
